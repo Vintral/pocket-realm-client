8a031344313aa04c5ba4ee49376196eaec563c1f
Vintral <jeffrey.heater@gmail.com>
2025-02-20 03:57:49 -0700

    Clean up data dumping on EventData

--- lib/data/event.dart
+++ lib/data/event.dart
@@ -1,31 +1,32 @@
 import 'package:logger/logger.dart';
 
 class EventData {
-  final Logger _logger = Logger( level: Level.trace );
-  
+  final Logger _logger = Logger();
+
   late String round;
   late String message;
   late String guid;
   late bool seen;
   late DateTime time;
-  
-  EventData( dynamic data ) {  
-    time = DateTime.parse( data[ "time" ] );
-    message = data[ "event" ] ?? "";
-    round = data[ "round" ] ?? "";
-    seen = data[ "seen" ] ?? "";    
-    guid = data[ "guid" ] ?? "";
 
-    dump();    
+  EventData(dynamic data) {
+    time = DateTime.parse(data["time"]);
+    message = data["event"] ?? "";
+    round = data["round"] ?? "";
+    seen = data["seen"] ?? "";
+    guid = data["guid"] ?? "";
+
+    dump();
   }
 
-  void dump() {    
-    _logger.t( "====================================" );
-    _logger.t( "GUID: $guid" );
-    _logger.t( "Message: $message" );
-    _logger.t( "Round: $round" );
-    _logger.t( "Seen: $seen" );        
-    _logger.t( "time: $time" );
-    _logger.t( "====================================" );
+  void dump() {
+    _logger.t("""
+====================================
+GUID: $guid
+Message: $message
+Round: $round
+Seen: $seen
+time: $time
+====================================""");
   }
 }
