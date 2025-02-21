68fc78394cbbd3e631ddd424a9768e5c3df0610d
Vintral <jeffrey.heater@gmail.com>
2025-02-21 11:24:01 -0700

    Revamp fields for RankingData

--- lib/data/ranking.dart
+++ lib/data/ranking.dart
@@ -1,34 +1,32 @@
 import 'package:logger/logger.dart';
 
 class RankingData {
-  // final Logger _logger = Logger(level: Logger.level);
   final Logger _logger = Logger();
 
-  late int place;
+  late int rank;
   late String username;
   late int avatar;
-  late int power;
-  late int land;
+  late int score;
 
   RankingData(dynamic data) {
-    place = 1;
+    rank = data["rank"] ?? 0;
     username = data["username"] ?? data["name"] ?? "";
     avatar = data["avatar"] is int
         ? data["avatar"]
         : int.tryParse(data["avatar"] ?? "0");
-    power = data["power"] ?? 0;
-    land = data["land"] ?? 0;
+    score = data["score"] is double
+        ? (data["score"] as double).floor()
+        : data["score"] as int;
 
     dump();
   }
 
   void dump() {
-    _logger.t("=================================");
-    _logger.t("Place: $place");
-    _logger.t("Username: $username");
-    _logger.t("Avatar: $avatar");
-    _logger.t("Power: $power");
-    _logger.t("Land: $land");
-    _logger.t("=================================");
+    _logger.t("""=================================
+Rank: $rank
+Username: $username
+Avatar: $avatar
+Score: $score
+=================================""");
   }
 }
