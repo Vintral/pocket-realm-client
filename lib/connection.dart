3fe4c26dfdbdeb01c8fb12565709fb47e8d01dbe
Vintral <jeffrey.heater@gmail.com>
2025-03-02 16:57:27 -0700

    Add add and remove contact calls to connection

--- lib/connection.dart
+++ lib/connection.dart
@@ -290,6 +290,21 @@ class Connection extends eventify.EventEmitter {
     _send({"type": "GET_PROFILE", "username": user});
   }
 
+  void addContact(String category, String guid, String note) {
+    _logger.i("addContact: $category $guid");
+    _send({
+      "type": "ADD_CONTACT",
+      "contact": guid,
+      "category": category,
+      "note": note
+    });
+  }
+
+  void removeContact(String category, String guid) {
+    _logger.i("removeContact: $category $guid");
+    _send({"type": "REMOVE_CONTACT", "contact": guid, "category": category});
+  }
+
   // void sendLogin( { String username, String password } ) {
   //   debug( "sendLogin" );
   //   _send( { 'command': 'login', 'username': username, 'password': password } );
